a0f6fcaf5ea29d8b0188ab2ae5132b0702c29b66
ErnieBuyco <buycoernie@gmail.com>
2019-06-05 11:05:16 +0800

    data-app

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: 'mailto:testemail@gmail.com',
+    image: emailIcon
+  },
+  {
+    id: 2,
+    link: 'https://github.com/ebuyco',
+    image: githubIcon
+  },
+  {
+    id: 3,
+    link: 'https://www.linkedin.com/in/erniebuyco',
+    image: linkedinIcon
+  },
+  {
+    id: 4,
+    link: 'https://twitter.com/erniebuyco',
+    image: twitterIcon
+  }
+];
+
+export default SOCIAL_PROFILES;
