3d6cff1cfb9bd608576a73da556ec84d8225529a
ErnieBuyco <buycoernie@gmail.com>
2019-06-05 15:04:51 +0800

    latest build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import App from './components/App';
 import Jokes from './components/Jokes';
@@ -11,7 +11,8 @@ import Header from './components/Header';
 import './index.css';
 
 ReactDOM.render(
-  <Router history={createBrowserHistory()}>
+  <BrowserRouter 
+  history={createBrowserHistory()}>
     <Switch>
       <Route exact path='/' render={() => <Header><App /></Header>} />
       <Route path='/jokes' render={() => <Header><Jokes /></Header>} />
@@ -19,6 +20,6 @@ ReactDOM.render(
       <Route path='/evens-or-odds' render={() => <Header><EvensOrOdds /></Header>} />
       <Route path='/reaction' render={() => <Header><Reaction /></Header>} />
     </Switch>
-  </Router>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
